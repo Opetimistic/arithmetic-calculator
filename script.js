160cc185ce6708cb192aa52a94c60c25efc53245
Opetimistic <akinwandeopeoluwa@yahoo.com>
2022-06-26 23:42:10 +0100

    improved- added while loop and conditions

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 
 alert("Welcome to a Basic Two-Input Arithmetic Calculator. \nFollow all the required instructions. \nKindly input the required information. ");
-alert("This calculator can only perform basic operations like Addition(+), Subtraction(-), Multiplication(*), & Division(/)")
+alert("This calculator can only perform basic operations like Addition(+), Subtraction(-), Multiplication(*), & Division(/)");
 
-let firstNumber = parseFloat(prompt("Enter the first number: "));
-let operator = prompt("Choose an operation to perform; ('+' for Add), ('-' for Subtract),\n('*' for Multiply), ('/' for Divide) ");
-let secondNumber = parseFloat(prompt("Enter the second number: "));
+let confirmCalculation = confirm(" Do you want to proceed?\nClick Ok to Continue OR Cancel to Exit.");
+if (confirmCalculation) {
 
-arithmeticOperation(firstNumber, operator, secondNumber);
+    let firstNumber = prompt("Enter the first number: ");
+    let operator = prompt("Choose an operation to perform; ('+' for Add), ('-' for Subtract),\n('*' for Multiply), ('/' for Divide) ");
+    let secondNumber = prompt("Enter the second number: ");
+
+    while (isNaN(firstNumber)|| firstNumber.trim()==="" || isNaN(secondNumber) || secondNumber.trim()==="") {
+        if (isNaN(firstNumber) || firstNumber.trim()=== "") {
+            firstNumber = prompt("Please enter a valid first number!");
+        }
+        else if (isNaN(secondNumber) || secondNumber.trim()=== "") {
+            secondNumber = prompt("Please enter a valid second number");
+        }
+        else {
+            break;
+        }
+    }
+
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    arithmeticOperation(firstNumber, operator, secondNumber);
+}
+else {
+    alert("Goodbye!");
+}
 
 function add(firstNumber, secondNumber) {
+    
     let result = firstNumber + secondNumber;
     alert(firstNumber +  " + " + secondNumber + " = " + result);
 }
